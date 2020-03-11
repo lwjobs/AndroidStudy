6f93dba5be35ea4753b235e363ffe9d1d2f8691d
lwjobs <532673600@qq.com>
2020-03-11 14:14:49 +0800

    add button click on ViewPager

--- ViewPagerTest/app/src/main/java/cn/edu/jssvc/viewpagertest/MainActivity.java
+++ ViewPagerTest/app/src/main/java/cn/edu/jssvc/viewpagertest/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private List<View> views;
     private MyNewAdapter myNewAdapter;
+
+    private Button button1,button2,button3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
         views.add(view1);
         views.add(view2);
         views.add(view3);
+
+        button1 = view1.findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"来自页面1",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2 = view2.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"来自页面2",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3 = view3.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"来自页面3",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         myNewAdapter = new MyNewAdapter(views);
         viewPager.setAdapter(myNewAdapter);
