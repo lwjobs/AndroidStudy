f979bb16035cfff6ac7e31e7a7a4e3af4292427e
lwjobs <532673600@qq.com>
2020-04-18 14:51:35 +0800

    modified Main2Activity.java

--- Login/app/src/main/java/cn/edu/jssvc/login/Main2Activity.java
+++ Login/app/src/main/java/cn/edu/jssvc/login/Main2Activity.java
@@ -8,15 +8,44 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private int count = 0;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        String password = intent.getStringExtra("password");
-        TextView textView = findViewById(R.id.textView3);
-        textView.setText(name+":"+password);
+        final TextView textView = findViewById(R.id.textView3);
+
+        progressBar = findViewById(R.id.progressBar2);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    count++;
+                    progressBar.setProgress(count);
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (count > 100) {
+//                        textView.setText(name + ":" + password);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = getIntent();
+                                String name = intent.getStringExtra("name");
+                                String password = intent.getStringExtra("password");
+                                textView.setText(name + ":" + password);
+                            }
+                        });
+                        break;
+                    }
+                }
+            }
+        }).start();
     }
 }
